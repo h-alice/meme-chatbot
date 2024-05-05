b48454c6c2383cee93eabc8eab83078efbc34d44
h-alice <admin@halice.art>
2024-05-05 14:34:00 +0800

    Generation parameter.

--- src/main.go
+++ src/main.go
@@ -11,20 +11,40 @@ const CHAT_TEMPLATE = `<start_of_turn>user
 `
 const CHAT_TEMPLATE_END = "<end_of_turn>"
 
-type LlmChatMessage struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
+type LlmGenerationParameters struct {
+	ModelName     string  `json:"model"`
+	Prompt        string  `json:"prompt"`
+	TopK          int     `json:"top_k"`
+	TopP          float64 `json:"top_p"`
+	RepeatPenalty float64 `json:"repeat_penalty"`
+	Temperature   float64 `json:"temperature"`
+	Stream        bool    `json:"stream"`
+	MaxTokens     int     `json:"max_tokens"`
 }
 
-type LlmGenerationParameters struct {
-	ModelName     string           `json:"model"`
-	Messages      []LlmChatMessage `json:"messages"`
-	TopK          int              `json:"top_k"`
-	TopP          float64          `json:"top_p"`
-	RepeatPenalty float64          `json:"repeat_penalty"`
-	Temperature   float64          `json:"temperature"`
-	Stream        bool             `json:"stream"`
-	MaxTokens     int              `json:"max_tokens"`
+// # Check and fix generation parameters
+//
+// This function checks the generation parameters and fixes them if needed.
+// If a parameter is missing or invalid, it is set to a default value.
+//
+// The default values are suggested by the llama-cpp-python library.
+// Check the `llama_cpp/server/types.py` source code for more information.
+func (lgp *LlmGenerationParameters) CheckAndFix() {
+	if lgp.TopK <= 0 {
+		lgp.TopK = 40
+	}
+	if lgp.TopP <= 0 || lgp.TopP > 1.0 {
+		lgp.TopP = 0.95
+	}
+	if lgp.RepeatPenalty <= 0 {
+		lgp.RepeatPenalty = 1.1
+	}
+	if lgp.Temperature <= 0 {
+		lgp.Temperature = 0.8
+	}
+	if lgp.MaxTokens <= 0 {
+		lgp.MaxTokens = 16
+	}
 }
 
 // # Prompt formatter
